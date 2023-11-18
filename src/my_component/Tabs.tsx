81b017bad0535651f6dc79b37dc4fe8d9390ede3
arianne05 <quimpoarianne5@gmail.com>
2023-11-18 18:50:00 +0800

    Layout for login react

--- src/my_component/Tabs.tsx
+++ src/my_component/Tabs.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {useState} from "react";
 import '../css/style.css';
 import image from '../img/main-logo.png';
+import dasmaLogo from '../img/dasma-logo.png';
 
 const Tabs = () => {
   const [toggleState, setToggleState] = useState(1);
@@ -24,12 +25,44 @@ const Tabs = () => {
       <div className="login-main">
         {/* Left Container */}
         <div className="login-left">
+          {/* Topbar */}
           <div className="top-bar-leftLogin">
             <img src={image} className="logo" alt="Image" />
             <select name="" className='position-drp'>
               <option value="">Admin</option>
               <option value="">Staff</option>
             </select>
+          </div>
+
+          <div className="login-form-container">
+            <div className="header">
+              <img src={dasmaLogo} alt="" className='dasmaLogo' />
+              <h2>BARANGAY DATU ESMAEL</h2>
+              <p>Health Center Record Management</p>
+            </div>
+
+            <div className="body">
+              <div className="sign-google">
+                <p>Log in with Google</p>
+              </div>
+              <hr className='lineDiv'/>
+              <div className="input-label">
+                <label htmlFor="Email">Email</label>
+                <input type="text" className='inputField' />
+              </div>
+
+              <div className="input-label">
+                <label htmlFor="Password">Password</label>
+                <input type="password" className='inputField' />
+                <div className="check-url">
+                  <label><input type="checkbox" />Remember Me</label>
+                  <a href="#">Forgot Password</a>
+                </div>
+              </div>
+              
+              <button className='btnLogin'>Log In</button>
+              <label className='noAccount'>Don't have an account yet? <a href="#">Sign Up</a></label>
+            </div>
           </div>
         </div>
 
